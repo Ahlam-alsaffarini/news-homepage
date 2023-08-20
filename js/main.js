f391a503f2216e10c370a30ab0c83c02737d7e6c
ahlam <ahlamalsaffarini@gmail.com>
2023-08-20 20:28:01 +0300

    modified css js html and add css.dark

--- js/main.js
+++ js/main.js
@@ -76,3 +76,105 @@ tlinks.onclick = function (e) {
   //stop propogation
   e.stopPropagation();
 };
+
+//light and dark mode
+
+let btn = document.querySelector(".button");
+btn.addEventListener("click", (ele) => {
+  if (ele.target.classList.contains("dark")) {
+    ele.target.classList.toggle("dark");
+    //so we in dark mode
+    theToggle(document.querySelector("body"));
+    if (document.querySelector(".container").classList.contains("sticky")) {
+      document.querySelector(".container").classList.remove("dark");
+    }
+
+    theToggle(document.querySelector(".logo img"));
+    theToggle(document.querySelector(".links-container .links"));
+    document.querySelectorAll(".toggle-menu span").forEach((e) => theToggle(e));
+    document
+      .querySelectorAll(".links-container .links li a")
+      .forEach((e) => theToggle(e));
+    theToggle(document.querySelector(".content .title h1"));
+    theToggle(document.querySelector(".content .txt p"));
+    theToggle(document.querySelector(".content .txt a"));
+    theToggle(document.querySelector(".content .news"));
+    theToggle(document.querySelector(".content .news h1"));
+    document
+      .querySelectorAll(".content .news .new-item a")
+      .forEach((e) => theToggle(e));
+    document
+      .querySelectorAll(".content .news .new-item p")
+      .forEach((e) => theToggle(e));
+
+    document
+      .querySelectorAll("footer .categories .catagories-content h1")
+      .forEach((e) => theToggle(e));
+    document
+      .querySelectorAll("footer .categories .catagories-content a")
+      .forEach((e) => theToggle(e));
+    document
+      .querySelectorAll("footer .categories .catagories-content p")
+      .forEach((e) => theToggle(e));
+  } else {
+    ele.target.classList.toggle("dark");
+    //we in light mode
+    theToggle(document.querySelector("body"));
+    if (document.querySelector(".container").classList.contains("sticky")) {
+      document.querySelector(".container").classList.add("dark");
+    }
+    theToggle(document.querySelector(".logo img"));
+    theToggle(document.querySelector(".links-container .links"));
+    document.querySelectorAll(".toggle-menu span").forEach((e) => theToggle(e));
+    document
+      .querySelectorAll(".links-container .links li a")
+      .forEach((e) => theToggle(e));
+    theToggle(document.querySelector(".content .title h1"));
+    theToggle(document.querySelector(".content .txt p"));
+    theToggle(document.querySelector(".content .txt a"));
+    theToggle(document.querySelector(".content .news"));
+    theToggle(document.querySelector(".content .news h1"));
+    document
+      .querySelectorAll(".content .news .new-item a")
+      .forEach((e) => theToggle(e));
+    document
+      .querySelectorAll(".content .news .new-item p")
+      .forEach((e) => theToggle(e));
+
+    document
+      .querySelectorAll("footer .categories .catagories-content h1")
+      .forEach((e) => theToggle(e));
+    document
+      .querySelectorAll("footer .categories .catagories-content a")
+      .forEach((e) => theToggle(e));
+    document
+      .querySelectorAll("footer .categories .catagories-content p")
+      .forEach((e) => theToggle(e));
+  }
+});
+
+function theToggle(ele) {
+  return ele.classList.toggle("dark");
+}
+
+//scroll
+window.onscroll = function () {
+  myFunction();
+};
+let header = document.querySelector(".header");
+let sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    if (tlinks.classList.contains("active")) {
+      header.classList.remove("sticky");
+      header.parentElement.classList.remove("sticky");
+    } else {
+      header.classList.add("sticky");
+      header.parentElement.classList.add("sticky");
+    }
+  } else {
+    header.classList.remove("sticky");
+    header.parentElement.classList.remove("sticky");
+  }
+}
